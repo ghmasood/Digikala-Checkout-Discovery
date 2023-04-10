3505493809476e150f961744950971d5aee29932
gh_masoud <msd.drg@gmail.com>
2023-04-10 20:00:05 +0330

    [ADD] product item card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import ProductItemCard from "./components/productItemCard";
 
 function App() {
-  return <ProductItemCard />;
+  return (
+    <>
+      <ProductItemCard
+        id={"3"}
+        title={"Microsoft Surface Laptop 4"}
+        image={"https://i.dummyjson.com/data/products/8/1.jpg"}
+        rating={4.43}
+        price={1499}
+        discount={8}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/components/productItemCard/index.tsx
+++ src/components/productItemCard/index.tsx
@@ -1,14 +1,43 @@
 import React from "react";
+import { BsStarFill } from "react-icons/bs";
+import "./Card.scss";
 
 interface IProductItemCardProps {
-  customClass?: string;
-  title?: string;
-  image?: string;
-  rating?: { rate: number; count: number };
-  count?: number;
+  id: string;
+  title: string;
+  image: string;
+  rating: number;
+  price: number;
+  discount: number;
 }
-function ProductItemCard({ customClass = "" }: IProductItemCardProps) {
-  return <div className={customClass}>ProductItemCard</div>;
+function ProductItemCard({
+  id,
+  title,
+  image,
+  rating,
+  price,
+  discount,
+}: IProductItemCardProps) {
+  return (
+    <div className={"card-item"}>
+      <img src={image} width="auto" alt={title} />
+      <div className="detail">
+        <span className="title">{title}</span>
+        <div className="rating">
+          <BsStarFill color="rgb(249, 188, 0)" /> {rating.toFixed(1)}
+        </div>
+        <div className="price-section">
+          <div className="price">
+            <span className={discount > 0 ? "sale" : ""}>${price}</span>
+            {discount > 0 && (
+              <span>${((price * (100 - discount)) / 100).toFixed(0)}</span>
+            )}
+          </div>
+          {discount > 0 && <span className="discount">{discount}%</span>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductItemCard;
