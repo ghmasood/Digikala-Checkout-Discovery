238f0f71f176dcae17b1e7623a1092d32dafb9ce
gh_masoud <msd.drg@gmail.com>
2023-05-01 18:14:52 +0330

    [REFACTOR] product cart design

--- src/components/productItemCard/index.tsx
+++ src/components/productItemCard/index.tsx
@@ -4,9 +4,10 @@ import styles from "./card.module.scss";
 
 import { BsCartPlusFill, BsStarFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { useAppDispatch } from "../../store/hook";
-import { addToCart } from "../../store/cart";
-import { productType } from "../../types";
+import { useAppDispatch } from "store/hook";
+import { productType } from "types";
+import { AddtoCart } from "components/icons";
+import { addToCart } from "store/cart";
 
 interface IProductItemCardProps {
   cardData: productType;
@@ -18,49 +19,39 @@ function ProductItemCard({ cardData }: IProductItemCardProps) {
     100
   ).toFixed(0);
   return (
-    <>
-      <div className={styles.cardItem}>
-        {/* <span className={styles.category}>{cardData.category}</span> */}
-        <Link to={`/product/?pid=${cardData.id}`}>
-          <img
-            src={cardData.thumbnail}
-            alt={cardData.title}
-            style={{ width: "100%", aspectRatio: "1.6", objectFit: "cover" }}
-          />
-          <div className={styles.detail}>
-            <span className={styles.title}>{cardData.title}</span>
-            <div className={styles.rating}>
-              <BsStarFill color="rgb(249, 188, 0)" />{" "}
-              {cardData.rating.toFixed(1)}
-            </div>
-            <div className={styles.priceSection}>
-              <div className={styles.price}>
-                <span
-                  className={cardData.discountPercentage > 0 ? styles.sale : ""}
-                >
-                  ${cardData.price}
-                </span>
-                {cardData.discountPercentage > 0 && <span>${FinalPrice}</span>}
-              </div>
-              {cardData.discountPercentage > 0 && (
-                <span className={styles.discount}>
-                  {cardData.discountPercentage.toFixed(0)}%
-                </span>
-              )}
-            </div>
-          </div>
-        </Link>
-        <button
-          className={styles.addBtn}
-          onClick={() => {
-            reduxDispatch(addToCart(cardData));
-          }}
-        >
-          <BsCartPlusFill />{" "}
-          <span style={{ fontWeight: "500" }}>Add to cart!</span>
-        </button>
+    <div className={styles.productCard}>
+      <div className={styles.productImage}>
+        <img
+          src={cardData.thumbnail}
+          height="160px"
+          width="200px"
+          style={{ borderRadius: "0.5rem" }}
+        />
       </div>
-    </>
+      <div className={styles.content}>
+        <span className={styles.title}>{cardData.title}</span>
+        <span className={styles.brand}>{cardData.brand}</span>
+        <div className={styles.action}>
+          <div
+            style={{
+              display: "flex",
+              gap: "0.25rem",
+              alignItems: "baseline",
+            }}
+          >
+            <span className={styles.price}>${FinalPrice}</span>
+            <span className={styles.oldPrice}>${cardData.price}</span>
+          </div>
+          <button
+            className={styles.btn}
+            onClick={() => reduxDispatch(addToCart(cardData))}
+          >
+            <AddtoCart />
+            {/* <span>Add To cart</span> */}
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import "./assets/styles/index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
