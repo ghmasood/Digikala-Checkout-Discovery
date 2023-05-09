02835936def5d0117237e21087091c4013db698f
ghmasood <msd.drg@gmail.com>
2023-05-09 23:11:05 +0330

    [ADD] badge on navBar cart when product added to cart

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styles from "./navbar.module.scss";
 import { icons } from "components/icons";
+import { useAppDispatch, useAppSelector } from "store/hook";
 
 function NavBar() {
   const loc = useLocation().pathname.slice(1);
-
+  const cartData = useAppSelector((store) => store.cart);
   return (
     <div className={styles.navroot}>
       <div className={styles.section}>
@@ -16,7 +17,8 @@ function NavBar() {
         <Link to={"/"} className={styles.item}>
           {loc === "" ? icons.homeActive() : icons.home()}
         </Link>
-        <Link to={"/cart"} className={styles.item}>
+        <Link to={"/cart"} className={styles.itemCart}>
+          {cartData.total > 0 && <div className={styles.badge} />}
           {loc === "cart" ? icons.cartActive() : icons.cart()}
         </Link>
       </div>
